1258ab0112eae3fc1e7f10372fad2489298cbe70
AnthonyGithubCorner <36902069+AnthonyGithubCorner@users.noreply.github.com>
2021-09-20 13:20:44 -0400

    Changed asset path to PATHTO

--- SDL2 Game/Scripts/Textures/TileInitLoader.cpp
+++ SDL2 Game/Scripts/Textures/TileInitLoader.cpp
@@ -17,185 +17,185 @@ bool loadTiles(SDL_Renderer* gRenderer)
 
 
     //Load tile texture
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_neutral.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_neutral.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_yellow.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_yellow.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_blue.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_blue.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_red.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_red.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_purple.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_purple.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Residential/Residential_rebel.png" ) )
+    if( !gTileTextures[tileConstants::RESIDENTIAL_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Residential/Residential_rebel.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
     //ECONOMIC BUILDING
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_Neutral.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_Neutral.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_Yellow.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_Yellow.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_Blue.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_Blue.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_Red.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_Red.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_Purple.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_Purple.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Economic/Market_rebel.png" ) )
+    if( !gTileTextures[tileConstants::ECONOMIC_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Economic/Market_rebel.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
     //SCIENCE BUILDING
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_Neutral.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_Neutral.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_Yellow.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_Yellow.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_Blue.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_Blue.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_Red.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_Red.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_Purple.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_Purple.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Science/Library_rebel.png" ) )
+    if( !gTileTextures[tileConstants::SCIENCE_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Science/Library_rebel.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
     //LORE BUILDING
-    if( !gTileTextures[tileConstants::LORE_BUILDING].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_neutral.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_neutral.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_yellow.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_yellow.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_blue.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_blue.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_red.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_red.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_purple.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_purple.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Lore/Museum_rebel.png" ) )
+    if( !gTileTextures[tileConstants::LORE_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Lore/Museum_rebel.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
     //RELIGION BUILDING
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_Neutral.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_Neutral.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_Yellow.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::ECONOMIC_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_Yellow.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_Blue.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::SCIENCE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_Blue.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_Red.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::LORE_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_Red.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_Purple.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::RELIGION_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_Purple.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/Religion/Temple_rebel.png" ) )
+    if( !gTileTextures[tileConstants::RELIGION_BUILDING + tileConstants::REBEL_CONTROLLED].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/Religion/Temple_rebel.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
@@ -204,13 +204,13 @@ bool loadTiles(SDL_Renderer* gRenderer)
     
     
     //UI TEXTURES
-    if( !gTileTextures[tileConstants::ALLOWED_TILE].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/UI/AllowedTile.png" ) )
+    if( !gTileTextures[tileConstants::ALLOWED_TILE].loadFromFile(gRenderer, "PATHTO/Tiles/UI/AllowedTile.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
     }
     
-    if( !gTileTextures[tileConstants::BLOCKED_TILE].loadFromFile(gRenderer, "/Users/anthony/C++Projects/SDL2 Game/SDL2 Game/Assets/Tiles/UI/BlockedTile.png" ) )
+    if( !gTileTextures[tileConstants::BLOCKED_TILE].loadFromFile(gRenderer, "PATHTO/Assets/Tiles/UI/BlockedTile.png" ) )
     {
         printf( "Failed to load texture!\n" );
         success = false;
